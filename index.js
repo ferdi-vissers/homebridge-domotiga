1cb9cbeb7e50bf9670f64efe18fd41af9154b288
Samfox2 <Samfox2@users.noreply.github.com>
2016-04-05 22:44:04 +0200

    Update index.js
    Eve custom characteristics/services added

--- index.js
+++ index.js
@@ -29,6 +29,57 @@ function Domotiga(log, config) {
     };
 }
 
+//Helpers
+var hexToBase64 = function (val) {
+    return new Buffer(('' + val).replace(/[^0-9A-F]/ig, ''), 'hex').toString('base64');
+}, base64ToHex = function (val) {
+    if (!val) return val;
+    return new Buffer(val, 'base64').toString('hex');
+}, swap16 = function (val) {
+    return ((val & 0xFF) << 8)
+    | ((val >> 8) & 0xFF);
+}, hexToHPA = function (val) {
+    return parseInt(swap16(val), 10);
+}, hPAtoHex = function (val) {
+    return swap16(Math.round(val)).toString(16);
+}, numToHex = function (val, len) {
+    var s = Number(val).toString(16);
+    if (s.length % 2 != 0) {
+        s = '0' + s;
+    }
+    if (len) {
+        return ('0000000000000' + s).slice(-1 * len);
+    }
+    return s;
+}
+
+function EveLogEntry(type, id, timestamp, sensorDatapoints) {
+    this.type = type;
+    this.id = id;
+    this.timestamp = timestamp;
+    if (type == 0x10) {
+        this.sensorDatapoints = sensorDatapoints;
+    } else {
+
+    }
+}
+
+EveLogEntry.prototype.toHex = function () {
+    var bytes = [this.type, this.id, 0x00, 0x00, 0x00, numToHex(parseInt(hPAtoHex(this.timestamp), 16), 4), 0x00, 0x00];
+
+    if (this.type == 0x10) {
+        bytes.push((this.sensorDatapoints.length * 2) + 1);
+        bytes.push.apply(bytes, this.sensorDatapoints.map(function (s) {
+            return hPAtoHex(s);
+        }));
+    }
+    var bytesAsStrings = bytes.map(function (s) { return typeof s === 'string' ? s : numToHex(s); });
+
+    // console.log(bytes, bytesAsStrings)
+    return new Buffer(bytesAsStrings.join(''), 'hex').toString('hex');
+}
+
+
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
@@ -105,7 +156,125 @@ module.exports = function (homebridge) {
     };
     inherits(EveAirPressure, Characteristic);
 
-       ////////////////////////////// Custom services //////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    EveUnknownCharacteristicE863F112 = function () {
+        Characteristic.call(this, 'EveUnknownCharacteristicE863F112', 'E863F112-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            desc: '',
+            perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE],
+            format: Characteristic.Formats.DATA,
+            value: hexToBase64('00000000')
+        });
+    };
+    inherits(EveUnknownCharacteristicE863F112, Characteristic);
+
+    EveUnknownCharacteristicE863F11E = function () {
+        Characteristic.call(this, 'EveUnknownCharacteristicE863F11E', 'E863F11E-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            desc: '',
+            perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE],
+            format: Characteristic.Formats.DATA,
+            value: hexToBase64('01be00be 00f44fb8 0a000000')
+        });
+    };
+    inherits(EveUnknownCharacteristicE863F11E, Characteristic);
+
+    EveUnknownCharacteristicE863F116 = function () {
+        Characteristic.call(this, 'EveUnknownCharacteristicE863F116', 'E863F116-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            desc: 'Read Trunk 1',
+            perms: [Characteristic.Perms.READ],
+            format: Characteristic.Formats.DATA,
+            value: hexToBase64('01010000 FF000000 3C0F0000 03010202 0203021D 00F50F00 00000000 000000')
+        });
+    };
+    inherits(EveUnknownCharacteristicE863F116, Characteristic);
+
+    EveUnknownCharacteristicE863F117 = function () {
+        Characteristic.call(this, 'EveUnknownCharacteristicE863F117', 'E863F117-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            desc: 'Read Trunk 2',
+            perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE],
+            format: Characteristic.Formats.DATA,
+            value: null
+        });
+    };
+    inherits(EveUnknownCharacteristicE863F117, Characteristic);
+
+    EveUnknownCharacteristicE863F11C = function () {
+        Characteristic.call(this, 'EveUnknownCharacteristicE863F11C', 'E863F11C-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            desc: 'Write Trunk 1',
+            perms: [Characteristic.Perms.WRITE],
+            format: Characteristic.Formats.DATA,
+            value: null
+        });
+    };
+    inherits(EveUnknownCharacteristicE863F11C, Characteristic);
+
+    EveUnknownCharacteristicE863F121 = function () {
+        Characteristic.call(this, 'EveUnknownCharacteristicE863F121', 'E863F121-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            desc: 'Write Trunk 2',
+            perms: [Characteristic.Perms.WRITE],
+            format: Characteristic.Formats.DATA,
+            value: null
+        });
+    };
+    inherits(EveUnknownCharacteristicE863F121, Characteristic);
+
+
+
+
+    //Some kind of internal temp sensor? Returns 18.7 which is eq to first saved temp - temp low maybe?
+    EveUnknownCharacteristicE863F111 = function () {
+        Characteristic.call(this, 'EveUnknownCharacteristicE863F111', 'E863F111-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            desc: 'Temp Min',
+            perms: [Characteristic.Perms.READ],
+            format: Characteristic.Formats.DATA,
+            value: null
+        });
+    };
+    inherits(EveUnknownCharacteristicE863F111, Characteristic);
+
+    //Some kind of internal temp sensor? Returns 21.01 - temp max maybe?
+    EveUnknownCharacteristicE863F124 = function () {
+        Characteristic.call(this, 'EveUnknownCharacteristicE863F124', 'E863F124-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            desc: 'Temp Max',
+            perms: [Characteristic.Perms.READ],
+            format: Characteristic.Formats.DATA,
+            value: null
+        });
+    };
+    inherits(EveUnknownCharacteristicE863F124, Characteristic);
+
+    //Some kind of internal humidity sensor? Returns 48.75 which is well in range of hum - hum low maybe?
+    EveUnknownCharacteristicE863F12A = function () {
+        Characteristic.call(this, 'EveUnknownCharacteristicE863F12A', 'E863F12A-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            desc: 'Hum Min',
+            perms: [Characteristic.Perms.READ],
+            format: Characteristic.Formats.DATA,
+            value: null
+        });
+    };
+    inherits(EveUnknownCharacteristicE863F12A, Characteristic);
+
+    //Some kind of internal humidity sensor? Returns 85.22 which is well in range of hum - hum max maybe?
+    EveUnknownCharacteristicE863F12B = function () {
+        Characteristic.call(this, 'EveUnknownCharacteristicE863F12B', 'E863F12B-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            desc: 'Hum Max',
+            perms: [Characteristic.Perms.READ],
+            format: Characteristic.Formats.DATA,
+            value: null
+        });
+    };
+    inherits(EveUnknownCharacteristicE863F12B, Characteristic);
+
+    ////////////////////////////// Custom services //////////////////////////////
     PowerMeterService = function (displayName, subtype) {
         Service.call(this, displayName, '00000001-0000-1777-8000-775D67EC4377', subtype);
         // Required Characteristics
@@ -135,8 +304,24 @@ module.exports = function (homebridge) {
         this.addOptionalCharacteristic(Characteristic.CurrentRelativeHumidity);
         this.addOptionalCharacteristic(Characteristic.CurrentTemperature);
         this.addOptionalCharacteristic(EveBatteryLevel);
+
+        this.addOptionalCharacteristic(EveUnknownCharacteristicE863F11E);
+        this.addOptionalCharacteristic(EveUnknownCharacteristicE863F112);
     };
     inherits(EveWeatherService, Service);
+
+    //Eve service (custom UUID)
+    EveWeatherLogService = function (displayName, subtype) {
+        Service.call(this, displayName, 'E863F007-079E-48FF-8F27-9C2605A29F52', subtype);
+        // Required Characteristics
+        // Optional Characteristics
+        this.addOptionalCharacteristic(EveUnknownCharacteristicE863F116);
+        this.addOptionalCharacteristic(EveUnknownCharacteristicE863F117);
+        this.addOptionalCharacteristic(EveUnknownCharacteristicE863F11C);
+        this.addOptionalCharacteristic(EveUnknownCharacteristicE863F121);
+    };
+    inherits(EveWeatherLogService, Service);
+    /////////////////////////////////////////////////////////////////////////////////////////////
 
     homebridge.registerAccessory("homebridge-domotiga", "Domotiga", Domotiga);
 }
@@ -499,6 +684,140 @@ Domotiga.prototype = {
             }
         }.bind(this));
     },
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    getValueE863F11E: function (callback) {
+        var that = this;
+        that.log("getting value for EveUnknownCharacteristicE863F11E");
+
+        var value = hexToBase64('01be00be 00f44fb8 0a000000');
+        callback(null, value);
+    },
+    setValueE863F11E: function (value, callback) {
+        var that = this;
+        that.log('[%s] was written: %s', 'EveUnknownCharacteristicE863F11E', base64ToHex(value));
+        callback(null, value);
+    },
+    getValueE863F112: function (callback) {
+        var that = this;
+        that.log("getting value for EveUnknownCharacteristicE863F112");
+        var value = hexToBase64('00000000'); // always the same
+        callback(null, value);
+    },
+    setValueE863F112: function (value, callback) {
+        var that = this;
+        that.log('[%s] was written: %s', 'EveUnknownCharacteristicE863F112', base64ToHex(value));
+        callback(null, value);
+    },
+    getValueE863F116: function (callback) {
+        var that = this;
+        that.log("getting value for EveUnknownCharacteristicE863F116 (Read Trunk 1)");
+        var value = hexToBase64('01010000 FF000000 3C0F0000 03010202 0203021D 00F50F00 00000000 000000');
+        callback(null, value);
+    },
+    getValueE863F117: function (callback) {
+        var that = this;
+        that.log("getting value for EveUnknownCharacteristicE863F117 (Read Trunk 2)");
+
+        /*
+        timestamp:
+        t0=0  = 02:00 o'clock
+        600   = 02:10 (t0 + 600s)
+        1200  = 02:20 (t0 + 1200s)
+        1800  = 02:30 (t0 + 1800s)
+        ...
+        3600  = 03.00
+        ...
+        72000 = 22:00
+        */
+
+        var value = hexToBase64(
+        '1500 0000 0000 0000 0080 0000 0000 0000 0000 0000 00' +
+        new EveLogEntry(0x10, 1, 0, [1870, 7214, 9900]).toHex()
+        + new EveLogEntry(0x10, 2, 600, [1810, 7214, 9901]).toHex()
+        + new EveLogEntry(0x10, 3, 1200, [1820, 7214, 9902]).toHex()
+        + new EveLogEntry(0x10, 4, 1800, [1830, 7214, 9903]).toHex()
+        + new EveLogEntry(0x10, 5, 2400, [1840, 7214, 9904]).toHex()
+        + new EveLogEntry(0x10, 6, 3000, [1850, 7214, 9905]).toHex()
+        + new EveLogEntry(0x10, 7, 7200, [1860, 7214, 9906]).toHex()
+        + new EveLogEntry(0x10, 8, 10800, [1870, 7214, 9907]).toHex()
+        + new EveLogEntry(0x10, 9, 14400, [1880, 7214, 9908]).toHex()
+        + new EveLogEntry(0x10, 10, 18000, [1890, 7214, 9909]).toHex()
+        + new EveLogEntry(0x10, 11, 21600, [1900, 7214, 9910]).toHex()
+        + new EveLogEntry(0x10, 12, 25200, [1910, 7214, 9911]).toHex()
+        + new EveLogEntry(0x10, 13, 28800, [1920, 7214, 9912]).toHex()
+        + new EveLogEntry(0x10, 14, 32400, [1930, 7214, 9913]).toHex()
+        + new EveLogEntry(0x10, 15, 36000, [1870, 7214, 9914]).toHex()
+        + new EveLogEntry(0x10, 16, 39600, [1870, 7214, 9915]).toHex()
+        + new EveLogEntry(0x10, 17, 43200, [1870, 7214, 9916]).toHex()
+        + new EveLogEntry(0x10, 18, 46800, [1870, 7214, 9917]).toHex()
+        + new EveLogEntry(0x10, 19, 50400, [1870, 7214, 9918]).toHex()
+        + new EveLogEntry(0x10, 20, 54000, [1870, 7214, 9919]).toHex()
+        + new EveLogEntry(0x10, 21, 57600, [1870, 7214, 9920]).toHex()
+        + new EveLogEntry(0x10, 22, 61200, [1870, 7214, 9921]).toHex()
+        + new EveLogEntry(0x10, 23, 64800, [1870, 7214, 9922]).toHex()
+        + new EveLogEntry(0x10, 24, 68400, [1870, 7214, 9923]).toHex()
+        + new EveLogEntry(0x10, 25, 69000, [1870, 7214, 9924]).toHex()
+        + new EveLogEntry(0x10, 26, 69600, [1870, 7214, 9925]).toHex()
+        + new EveLogEntry(0x10, 27, 70200, [1870, 7214, 9926]).toHex()
+        + new EveLogEntry(0x10, 28, 72000, [1870, 7214, 9927]).toHex()
+        );
+        callback(null, value);
+    },
+    setValueE863F117: function (value, callback) {
+        var that = this;
+        that.log('[%s] was written: %s', 'EveUnknownCharacteristicE863F117', base64ToHex(value));
+        callback(null, value);
+    },
+    getValueE863F11C: function (callback) {
+        var that = this;
+        that.log("getting value for EveUnknownCharacteristicE863F11C");
+        callback(null, null); //todo: return value
+    },
+    getValueE863F121: function (callback) {
+        var that = this;
+        that.log("getting value for EveUnknownCharacteristicE863F121");
+        callback(null, null); //todo: return value
+    },
+    setValueE863F121: function (value, callback) {
+        var that = this;
+        that.log('[%s] was written: %s', 'EveUnknownCharacteristicE863F121', base64ToHex(value));
+        callback(null, value);
+    },
+    //Temp low?
+    getValueE863F111: function (callback) {
+        var that = this;
+        that.log("getting value for EveUnknownCharacteristicE863F111 Temp low");
+        var value = hexToBase64('4e07'); // Returns 18.7
+        callback(null, value);
+    },
+    //Temp high?
+    getValueE863F124: function (callback) {
+        var that = this;
+        that.log("getting value for EveUnknownCharacteristicE863F124 Temp high");
+        var value = hexToBase64('3508'); // Returns 21.01
+        callback(null, value);
+    },
+    //Hum low?
+    getValueE863F12A: function (callback) {
+        var that = this;
+        that.log("getting value for EveUnknownCharacteristicE863F12A Hum low");
+        var value = hexToBase64('0b13'); // Returns 48.75
+        callback(null, value);
+    },
+    //Hum high?
+    getValueE863F12B: function (callback) {
+        var that = this;
+        that.log("getting value for EveUnknownCharacteristicE863F12B Hum high");
+        var value = hexToBase64('4a21'); // Returns 85.22
+        callback(null, value);
+    },
+    /////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
     getServices: function () {
         // You can OPTIONALLY create an information service if you wish to override
         // the default values for things like serial number, model, etc.
@@ -576,6 +895,12 @@ Domotiga.prototype = {
                         .on('get', this.getCurrentAirPressure.bind(this));
                 break;
 
+            case "FakeEveWeatherSensorWithLog":
+                primaryservice = new EveWeatherService("Eve Weather");
+                primaryservice.getCharacteristic(EveAirPressure)
+                        .on('get', this.getCurrentAirPressure.bind(this));
+                break;
+
             case "Powermeter":
                 primaryservice = new PowerMeterService(this.config.service);
                 primaryservice.getCharacteristic(EvePowerConsumption)
@@ -586,11 +911,13 @@ Domotiga.prototype = {
                 this.log('Service %s %s unknown, skipping...', this.config.service, this.config.name);
                 break;
         }
+
         services = services.concat(primaryservice);
         if (services.length === 1) {
             this.log("WARN: Only the InformationService was successfully configured for " + this.config.name + "! No device services available!");
             return services;
         }
+
         // Everything outside the primary service gets added as optional characteristics...
         var service = services[1];
 
@@ -616,7 +943,8 @@ Domotiga.prototype = {
                     .on('get', this.getOutletInUse.bind(this));
         }
         // Eve characteristic (custom UUID)
-        if (this.config.valueAirPressure &&  (this.config.service != "FakeEveWeatherSensor")) {
+        if (this.config.valueAirPressure && 
+        (this.config.service != "FakeEveWeatherSensor") && (this.config.service != "FakeEveWeatherSensorWithLog")) {
             service.addCharacteristic(EveAirPressure)
                     .on('get', this.getCurrentAirPressure.bind(this));
         }
@@ -635,6 +963,56 @@ Domotiga.prototype = {
         if (this.config.valueTotalPowerConsumption) {
             service.addCharacteristic(EveTotalPowerConsumption)
                     .on('get', this.getEveTotalPowerConsumption.bind(this));
+        }
+        
+        // Playground
+        if (this.config.service == "FakeEveWeatherSensorWithLog") {
+            service.addCharacteristic(EveUnknownCharacteristicE863F11E)
+                            .on('get', this.getValueE863F11E.bind(this))
+                            .on('set', this.setValueE863F11E.bind(this));
+
+            service.addCharacteristic(EveUnknownCharacteristicE863F112)
+            .on('get', this.getValueE863F112.bind(this))
+            .on('set', this.setValueE863F112.bind(this));
+
+            //Temp low?
+            service.addCharacteristic(EveUnknownCharacteristicE863F111)
+            .on('get', this.getValueE863F111.bind(this));
+
+            //Temp high?
+            service.addCharacteristic(EveUnknownCharacteristicE863F124)
+            .on('get', this.getValueE863F124.bind(this));
+
+            //Hum low?
+            service.addCharacteristic(EveUnknownCharacteristicE863F12A)
+            .on('get', this.getValueE863F12A.bind(this));
+
+            //Hum high?
+            service.addCharacteristic(EveUnknownCharacteristicE863F12B)
+            .on('get', this.getValueE863F12B.bind(this));
+
+            var logService = new EveWeatherLogService("Eve Weather log service");
+            //Eve characteristic (custom UUID)
+            // read trunk 1
+            logService
+            .addCharacteristic(EveUnknownCharacteristicE863F116)
+            .on('get', this.getValueE863F116.bind(this));
+
+            // read trunk 2
+            logService
+            .addCharacteristic(EveUnknownCharacteristicE863F117)
+            .on('get', this.getValueE863F117.bind(this))
+            .on('set', this.setValueE863F117.bind(this));
+
+            logService.addCharacteristic(EveUnknownCharacteristicE863F11C)
+            .on('get', this.getValueE863F11C.bind(this));
+
+            logService
+            .addCharacteristic(EveUnknownCharacteristicE863F121)
+            .on('get', this.getValueE863F121.bind(this))
+            .on('set', this.setValueE863F121.bind(this));
+
+            services = services.concat(logService);
         }
         return services;
     }
